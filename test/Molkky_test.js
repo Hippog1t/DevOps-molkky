e4c4db51656f4ebdc2518e9420a27c77e64ab70a
hippog1t <hippolytefoussat@hotmail.fr>
2020-01-21 17:30:24 +0100

    added 2 tests, 3 done

--- test/Molkky_test.js
+++ test/Molkky_test.js
@@ -2,11 +2,24 @@ require('chai').should();
 const {Molkky} = require('../Molkky.js');
 
 describe('Molkky.js', () => {
-    const m = new Molkky();
+
     it('score should be at 0 at first', () => {
+        const m = new Molkky();
         m.getScore().should.be.equal(0);
     });
-    // it('score should be at 0 at first', () => {
-    //     m.getScore().should.be.equal(0);
-    // });
+    it('score of player should be equal to number of pin', () => {
+        const m = new Molkky();
+        m.oneDown(9);
+        m.getScore().should.be.equal(9);
+    });
+    it('after 2 throws score of player should be equal to number of pins added', () => {
+        const m = new Molkky();
+        m.nDown(3);
+        m.getScore().should.be.equal(3);
+    });
+    it('victory if score is 50', () => {
+        const m = new Molkky();
+        m.nDown(50);
+        m.victory.should.be.true;
+    });
 });
