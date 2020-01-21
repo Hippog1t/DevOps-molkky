e8dd9080801dbee5de6b5eed23bd9192d9a0cf7b
lacour_a <alexandre.lacour3@hotmail.fr>
2020-01-21 17:32:14 +0100

    added basic functions on molkky.js

--- Molkky.js
+++ Molkky.js
@@ -1,10 +1,36 @@
 class Molkky{
 	constructor(){
-
+		this.score = 0;
+		this.victory = false;
+		this.eliminated = false;
+		this.n_fails = 0
 	}
 
 	getScore(){
-		return 0;
+		return this.score;
+	}
+
+	oneDown(number){
+		this.score += number;
+		check()
+	}
+
+	nDown(n_down){
+		this.score += n_down;
+		check()
+	}
+
+	check(){
+		if(this.score > 50){
+			this.score = 25
+		}
+		else if(this.score == 50){
+			this.victory = true;
+		}
+
+		if(this.n_fails >= 3){
+			this.eliminated = true;
+		}
 	}
 }
 
